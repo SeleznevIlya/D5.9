d2229b014feec9e25fbf865e427c8a5f9a3a0141
Helfik <helfik123_1998@mail.ru>
2022-10-14 00:19:21 +0300

    Почти довёл фильтр до ума, осталось разобраться с регистрами

--- DjangoLearningDB/LearnModels/templatetags/custom_filters.py
+++ DjangoLearningDB/LearnModels/templatetags/custom_filters.py
@@ -1,6 +1,53 @@
-def main():
+from django import template
+
+"""FlashText - библиотека для замены слов в тексте
+from flashtext import KeywordProcessor
+keyword_processor = KeywordProcessor()
+keyword_processor.add_keywords_from_dict(CURRENCY_WORDS)
+
+CURRENCY_WORDS = {
+     'р***': ['редиска'],
+     'д***': ['директор'],
+     'п***': ['пропускать'],
+}"""
+
+
+class CensorException(Exception):
     pass
 
 
-if __name__ == '__main__':
-    main()
+register = template.Library()
+
+
+
+CURRENCY_WORDS = {
+    'редиска': 'редиска',
+    'директор': 'директор',
+    'пропускать': 'пропускать',
+    'глубине': 'глубине',
+    'поразили': 'поразили'
+}
+
+
+
+@register.filter()
+def censor(value: str) -> str:
+    try:
+        if not isinstance(value, str):
+            raise CensorException('Error')
+
+        a = value
+        for word in list(CURRENCY_WORDS.keys()):
+            if word.lower() in a:
+                a = a.replace(word, f'{word[:1]}*****')
+
+        return a
+        """Нашёл альтернативный способ как заменить слова в тексте.
+        Использование этой библиотеки оправдано в случае если тексты огромные.
+        За счёт заложенный алгоритмов не нужно на каждой итерации проходить по каждому слову теста.
+        Вместо этого алгоритм по тексту пробегает всего 1 раз"""
+        # new_value = keyword_processor.replace_keywords(value)
+        # return new_value
+
+    except CensorException as e:
+        print(e)
