46f5cdfbee4ee3659302ee59b3101a00480d0e6b
Helfik <helfik123_1998@mail.ru>
2022-10-23 19:33:53 +0300

    Реализован виджет календаря для выбора даты

--- DjangoLearningDB/LearnModels/filters.py
+++ DjangoLearningDB/LearnModels/filters.py
@@ -1,20 +1,18 @@
-import django_filters
-from django_filters import FilterSet
+from django.forms import DateInput
+from django_filters import FilterSet, DateFilter
 from .models import Post
 
 
 class PostFilter(FilterSet):
     # author__id_user_id__username = django_filters.CharFilter(lookup_expr='icontains')
+    """Date_time выводит виджет календаря при выборе даты за счёт атрибуты widget"""
+    date_time = DateFilter(lookup_expr='gt', widget=DateInput(attrs={'type': 'date'}))
+
     class Meta:
         model = Post
 
         fields ={
             'author__id_user_id__username': ['icontains'],
-            'header': '',
-            'date_time': ['gt']
+            'header': ['icontains'],
+
         }
-        # fields = [
-        #     'author__id_user_id__username',
-        #     'header',
-        #     #'date_time'
-        # ]
