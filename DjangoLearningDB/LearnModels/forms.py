b51cbdb6dea7533004a6cb236f33f256fb561f83
Helfik <helfik123_1998@mail.ru>
2022-10-30 12:46:42 +0300

    При регистрации пользователь добавляется в группу common

--- DjangoLearningDB/LearnModels/forms.py
+++ DjangoLearningDB/LearnModels/forms.py
@@ -1,6 +1,8 @@
 from .models import Post
 from django import forms
 from django.core.exceptions import ValidationError
+from allauth.account.forms import SignupForm
+from django.contrib.auth.models import Group
 
 
 class PostForm(forms.ModelForm):
@@ -30,3 +32,11 @@ class PostForm(forms.ModelForm):
                 "header": "Error: пустой текст"
             })
         return cleaned_data
+
+class BasicSignupForm(SignupForm):
+
+    def save(self, request):
+        user = super(BasicSignupForm, self).save(request)
+        common_group = Group.objects.get(name='common')
+        common_group.user_set.add(user)
+        return user
